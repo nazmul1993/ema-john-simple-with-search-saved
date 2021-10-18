f9286cdfe1fb5cd2fe1ec90270a3406f28d3d734
nazmul1993 <md.islam.nazmul@stud.uni-due.de>
2021-10-18 23:27:05 +0200

    last commit

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>404</h1>
+            <h3>
+                The page you are ooking for not found
+            </h3>
+        </div>
+    );
+};
+
+export default NotFound;
---
+++ src/components/PlaceOrder/PlaceOrder.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import img from '../../images/giphy.gif';
+
+const PlaceOrder = () => {
+    return (
+        <div>
+            <img src={img} alt=" "/>
+
+            
+        </div>
+    );
+};
+
+export default PlaceOrder;
